f3a9721720b8d2893851490060778b6050552f28
abbeyLC <alc16j@my.fsu.edu>
2019-10-04 13:17:22 -0400

    Add mileage estimate for the Boat class
    Mileage estimate for the boat type depends on the time, wave
    height (a random number between 1 and 100), and the boat weight,
    which is a member variable for every Boat object.
    
    Light boats are classified as those <= 10000 lbs. Heavy boats are
    classified as those > 10000 lbs. The wave height is considered
    high if the randomly generated number is > 50, and low if <= 50.
    
    The if else statements in the Boat class's mileage estimate function
    simulate that 1) light boats are more likely to travel faster than
    heavier boats if the wave height is low, and 2) light boats are more
    likely to travel slower than heavier boats if the wave height is high.
    
    Every boat, no matter the wave height or weight, begins with a mileage
    of 0.05 * time. Then, depending on the wave height and weight, a
    certain percentage of that mileage is added to the final mileage.
    
    If the waves are small and the boat is light, 50%-100% of the mileage
    is added because these conditions will allow the boat to go faster.
    If the waves are big and the boat is light, 1%-50% of the mileage
    is added because these are conditions that will make the boat go
    slower. 1%-50% is always added to a heavy boat because it will not be
    affected by the waves, and heavier boats are assumed to always go slower.

--- Boat.cpp
+++ Boat.cpp
@@ -55,9 +55,47 @@ void Boat::setWeight(int w)
 }
 
 // estimate and return mileage
-double Boat::mileageEstimate(double time)
+double Boat::mileageEstimate(double t)
 {
-	return 0;
+	// mileage is 0.05 miles per minute
+	double mileage = t * 0.05;
+
+	// simulate waves height between 1 and 100
+	double waveHeight = rand() % 100 + 1;
+
+	/*if statements simulate the following:
+		- light boats travel faster in small waves
+		- light boats travel slower in large waves
+
+		- heavy boats travel fast in choppy waves
+		- heavy boats travel 
+	*/
+
+	// if small waves and light boat -- faster
+	if (waveHeight =< 50 && weight <= 10000)
+	{
+		// add random percentage of mileage between 50 and 100 to simulate that the boat can go
+		//		faster in these conditions
+		mileage += (rand() % 100 + 50)*mileage;
+	}
+
+	// if big waves and light boat -- slower
+	else if (waveHeight > 50 && weight <= 10000)
+	{
+		// add random percentage of mileage between 50 and 100 to simulate that the boat will go
+		//		slower in these conditions
+		mileage += (rand() % 50 + 1)*mileage;
+	}
+
+	// heavy boats will always go slower
+	else
+	{
+		// add random percentage of mileage between 1 and 50 to simulate that heavy boats travel slow
+		mileage += (rand() %  50 + 1)*mileage;
+	}
+
+	return mileage;
+
 }
 
 // used to output details of the Boat boat
